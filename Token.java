8f7d544c3837f24ed91574f9cea1ce411fa6f0eb
Yuram Almeida <yuramalmeida1@gmail.com>
2026-09-13 16:33:54 -0300

    Add files via upload
    testando aqui

---
+++ Token.java
@@ -0,0 +1,16 @@
+package br.ufma.ecp;
+
+public class Token {
+    final TokenType type;
+    final String lexeme;
+
+    public Token(TokenType type, String lexeme) {
+        this.type = type;
+        this.lexeme = lexeme;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + type + ">" + lexeme + "</" + type + ">";
+    }
+}
